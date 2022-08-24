d25eb7fdcc9cfff25b1f2590f32be2d8c37d7d8a
FieBee <phuonglf98@gmail.com>
2022-08-24 21:58:33 +0700

    add Th MenuNgang

---
+++ Th-MenuNgang/src/main/java/com/example/thmenungang/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.thmenungang;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
